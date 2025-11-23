ce7ff33cb063dc6f6097d71b663e1696d0446f7b
austinrevera01-cyber <austinrevera01@gmail.com>
2025-11-23 13:19:11 -0600

    Delete counts_to_wheel_angle.m

--- counts_to_wheel_angle.m
+++
@@ -1,5 +0,0 @@
-function theta = counts_to_wheel_angle(counts, params)
-    motor_rad = counts * (2*pi / params.encoder.counts_per_rev);
-    theta = motor_rad / params.gear.N;
-    theta = wrap_angle(theta, [0 .36]);
-end
