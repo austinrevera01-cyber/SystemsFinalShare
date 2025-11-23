77618d4b7175693f0089268b35d3f0a47e02f7b1
austinrevera01-cyber <austinrevera01@gmail.com>
2025-11-23 13:19:48 -0600

    Delete steering.m

--- steering.m
+++
@@ -1,9 +0,0 @@
-function G = steering(params)
-    s = tf('s');
-    J = params.motor.J;
-    B = params.motor.B;
-    N = params.gear.N;
-    Kt = params.vehicle.Kt;
-
-    G = (N*Kt)/(J*s^2 + B*s);
-end
